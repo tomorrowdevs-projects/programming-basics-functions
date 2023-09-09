c4c57f8599772a84d708cddb9089ff892308525e
rosario <rosariospalla.job@gmail.com>
2023-09-10 01:26:22 +0200

    fix: trmoved array from the logic of the code

--- projects/039-random-password/js/index.js
+++ projects/039-random-password/js/index.js
@@ -1,12 +1,12 @@
 function getPassword() {
-    const password = [];
-    password.length = Math.floor((Math.random() * 4) + 7);
+    let password = "";
+    const passwordLength = Math.floor((Math.random() * 4) + 7);
 
-    for (let i = 0; i < password.length; i++) {
-        password[i] = String.fromCharCode(Math.floor((Math.random() * 94) + 33))
+    for (let i = 0; i < passwordLength; i++) {
+        password += String.fromCharCode(Math.floor((Math.random() * 94) + 33))
     }
 
-    return password.join("");
+    return password;
 }
 
 console.log( getPassword() );
