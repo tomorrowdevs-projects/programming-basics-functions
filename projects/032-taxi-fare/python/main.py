5e24e0b375642b5dc5d78160996dafa2963c3759
Matteo Muceli <muceteo@gmail.com>
2023-09-21 16:54:52 +0100

    Update main.py

--- projects/032-taxi-fare/python/main.py
+++ projects/032-taxi-fare/python/main.py
@@ -3,8 +3,6 @@ def calculate_distance(kilometers_travelled):
 
     conversion_metres = kilometers_travelled * 1000
     service_price += (conversion_metres / 140) * 0.25
+    service_price = "{:.2f}".format(service_price)
 
-    return print("{:.2f}".format(service_price))
-
-
-
+    return service_price
