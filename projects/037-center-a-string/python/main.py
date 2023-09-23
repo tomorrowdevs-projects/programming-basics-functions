2d3cc5f9c977c6ba3ef454e42bb8df43f2d827bf
Daniele <danieleslvstrn@gmail.com>
2023-09-23 15:36:27 +0200

    solution-M01-37-center-a-string

---
+++ projects/037-center-a-string/python/main.py
@@ -0,0 +1,17 @@
+def centered_string(string, window_width):
+    string_width = len(string)
+
+    if string_width >= window_width:
+        return f'The width of string is larger then window\'s size : {string_width}'
+    else:
+        leading_space = (window_width - string_width) // 2
+        centered_string = ' ' * leading_space + string + ' ' * leading_space
+
+    return centered_string
+    
+def main():
+    user_string = input('Type a string: ')
+    window_width = int(input('Type window\'s width: '))
+    print(centered_string(user_string,window_width))
+
+main()
