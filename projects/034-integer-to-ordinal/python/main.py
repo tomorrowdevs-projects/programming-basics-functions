a61cc217468e327af15dc90d3c9481c49770e756
Matteo Muceli <muceteo@gmail.com>
2023-09-25 17:30:09 +0100

    Update main.py

--- projects/034-integer-to-ordinal/python/main.py
+++ projects/034-integer-to-ordinal/python/main.py
@@ -1,7 +1,15 @@
 def get_ordinal_number(integer):
-    ordinal_numbers = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
-    
+    ordinal_numbers = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth",
+                       "eleventh", "twelfth"]
+
     if 1 <= integer <= 12:
         return ordinal_numbers[integer - 1]
     else:
         return ""
+
+def main():
+    for number in range(1, 13):
+        print(f"{number}-> {get_ordinal_number(number)}")
+
+if __name__ == '__main__':
+    main()
