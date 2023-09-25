3c2470ec0660e635d03f766fce215311196278c4
Matteo Muceli <muceteo@gmail.com>
2023-09-25 17:02:08 +0100

    Update main.py

--- projects/032-taxi-fare/python/main.py
+++ projects/032-taxi-fare/python/main.py
@@ -6,3 +6,11 @@ def calculate_distance(kilometers_travelled):
     service_price = "{:.2f}".format(service_price)
 
     return service_price
+
+def main():
+    distance = float(input("Enter the distance traveled in kilometers: "))
+    taxi_fare = calculate_distance(distance)
+    print(f'{taxi_fare} $')
+
+if __name__ == '__main__':
+    main()
