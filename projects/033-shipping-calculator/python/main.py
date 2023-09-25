9ef2e94dc6807c7827e114f348dfa2e147fbe92e
Matteo Muceli <muceteo@gmail.com>
2023-09-25 17:08:13 +0100

    Update main.py

--- projects/033-shipping-calculator/python/main.py
+++ projects/033-shipping-calculator/python/main.py
@@ -7,3 +7,11 @@ def shipping_calculator(number_of_items):
         shipping_charge += 2.99
 
     return shipping_charge
+
+def main():
+    number_of_items = int(input('Enter the number of items purchased: '))
+    shipping_charge = shipping_calculator(number_of_items)
+    print(f"{shipping_charge:.2f} $")
+
+if __name__ == '__main__':
+    main()
