c00ebd4e6351e74756c431b7a6688c29b5e8df05
Matteo Muceli <muceteo@gmail.com>
2023-09-26 17:00:53 +0100

    Update main.py

--- projects/037-center-a-string/python/main.py
+++ projects/037-center-a-string/python/main.py
@@ -5,3 +5,12 @@ def center_a_string(s, w):
         spaces_number = (w - len(s)) // 2
         new_string = " " * spaces_number + s
         return new_string
+
+def main():
+    user_string = input("Enter your string here:\n")
+    window_width = int(input('Enter the width of the window:\n'))
+    centered_string = center_a_string(user_string, window_width)
+    print(centered_string)
+
+if __name__ == '__main__':
+    main()
