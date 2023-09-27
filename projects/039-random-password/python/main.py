d178599f945c6c972c8e21e63ebf991c4da38ef9
Matteo Muceli <muceteo@gmail.com>
2023-09-27 19:36:27 +0100

    Update main.py

--- projects/039-random-password/python/main.py
+++ projects/039-random-password/python/main.py
@@ -1,8 +1,16 @@
-import string
 import random
 
 def random_password():
-    characters = string.ascii_letters + string.punctuation + string.digits
+    characters = []
     password_length = random.choice(range(7, 11))
 
+    for character in range(33, 127):
+        characters.append(str(chr(character)))
     return ''.join(random.choices(characters, k = password_length))
+
+def main():
+    password = random_password()
+    print(password)
+
+if __name__ == '__main__':
+    main()
