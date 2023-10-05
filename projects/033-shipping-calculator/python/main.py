69a95c8fd2b42e98151a095933af02fb90a577f7
Daniele <danieleslvstrn@gmail.com>
2023-10-05 18:11:20 +0200

    fix incorrect result and some comments

--- projects/033-shipping-calculator/python/main.py
+++ projects/033-shipping-calculator/python/main.py
@@ -1,15 +1,14 @@
 # defines a function to calculate shipping costs
 def shipping_cost(items):
-# conditional that returns the shipping costs if items is equal to 1, display a error message if it is equal to 0, else returns the total price.
-    if items == '1':
+# conditional that returns the shipping costs based on the number of item purchased
+    items = int(items)
+    if items == 1:
         return 10.99
-    elif items == '0':
-        return 'error'
     else: 
-        total = 10.99 + (int(items) * 2.99)
+        total = 10.99 + (items * 2.99)
         return total
 
-# defines a main function that asks to user the number of items purchased, and return shpping costs
+# defines a main function that asks to user the number of items, and return shpping costs
 def main():
     user_input = input('Insert items: ')
     if user_input == '0':
@@ -20,3 +19,4 @@ def main():
 # sets a conditional statement so the program run main() only if has not been imported in a another file.
 if __name__ == '__main__':
     main()
+
