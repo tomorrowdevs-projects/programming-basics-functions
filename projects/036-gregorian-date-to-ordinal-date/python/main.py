253033c83acf52b9daa5dafa621b5068cc85f2a0
Daniele <danieleslvstrn@gmail.com>
2023-10-07 18:17:20 +0200

    solution-gregorian-date-to-ordinal-date

---
+++ projects/036-gregorian-date-to-ordinal-date/python/main.py
@@ -0,0 +1,44 @@
+
+def gregorian_date_to_ordinal(day,month,year):
+
+    if year % 400 == 0:
+        leap_year = True
+    elif year % 400 != 0 and year % 100 != 0 and year % 4 == 0:
+        leap_year = True
+    else:
+        leap_year = False
+
+    if (day > 28 and month == 2 and leap_year == False) or (day > 29 and month == 2 and leap_year == True) or (day > 31 and (1 <= month <= 7 and month % 2 != 0)) or (day > 31 and (7 < month <= 12 and month % 2 == 0)) or (day > 30 and (1 <= month <= 7 and month % 2 == 0)) or  (day > 30 and (7 < month <= 12 and month % 2 != 0)) or  month > 12 or month < 1 :
+        return 'invalid date'
+    else:    
+        ordinal_day = 0
+
+        if month == 1:
+            ordinal_day = day
+        else:
+            for i in range(1, month):
+                if i == 2 and leap_year == True: 
+                    ordinal_day += 29
+                elif i == 2 and leap_year == False:
+                    ordinal_day += 28 
+                elif 1 <= i <= 7 and i % 2 != 0:
+                    ordinal_day += 31
+                elif 7 < i <= 12 and i % 2 == 0:
+                    ordinal_day += 31
+                elif 1 <= i <= 7 and i % 2 == 0:
+                    ordinal_day += 30
+                elif 7 < i <= 12 and i % 2 != 0:
+                    ordinal_day += 30
+            ordinal_day += day
+
+        return f'{year} - {ordinal_day}'
+
+def main():
+    
+    user_day = int(input('Enter a day: '))
+    user_month = int(input('Enter a month: '))
+    user_year = int(input('Enter a year: '))
+    print(gregorian_date_to_ordinal(user_day,user_month,user_year))
+
+if __name__ == "__main__":
+    main()
