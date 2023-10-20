cada3cccb1674f4338a9e1db184a8436017f9571
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-10-20 20:03:15 +0200

    Added solution and comments

--- projects/005-gregorian-date-to-ordinal-date/js/index.js
+++ projects/005-gregorian-date-to-ordinal-date/js/index.js
@@ -1,17 +1,54 @@
 'use strict';
+function gregorian_to_ordinal_date(day, month, year) {
+    const ordinal_date_regular_years =
+        30 * (month - 1) + Math.trunc(0.6 * (month + 1)) - 3 + day; // I used the algorithm of the so-called Zeller's congruence method.
 
-const date = Number(prompt('Enter an integer between 1 to 31'));
+    const ordinal_date_leap_years =
+        30 * (month - 1) + Math.trunc(0.6 * (month + 1)) - 2 + day;
+
+    const leapYear =
+        (year % 4 === 0 && year % 100 !== 0) ||
+        (year % 4 === 0 && year % 100 === 0 && year % 400 === 0);
+    // First condition if it is a leap year
+    if (
+        (day <= 31 &&
+            year === leapYear &&
+            ((month <= 7 && month % 2 === 1) ||
+                month === 8 ||
+                month === 10 ||
+                month === 12)) ||
+        (day <= 30 &&
+            year === leapYear &&
+            (month === 4 || month === 6 || month === 9 || month === 11)) ||
+        (day <= 29 && month === 2 && year === leapYear)
+    ) {
+        console.log(
+            `The date entered corresponds to day no. ${ordinal_date_leap_years}.`
+        );
+        // Second condition if it is not a leap year
+    } else if (
+        (day <= 31 &&
+            year !== leapYear &&
+            ((month <= 7 && month % 2 === 1) ||
+                month === 8 ||
+                month === 10 ||
+                month === 12)) ||
+        (day <= 30 &&
+            year !== leapYear &&
+            (month === 4 || month === 6 || month === 9 || month === 11)) ||
+        (day <= 28 && month === 2 && year !== leapYear)
+    ) {
+        console.log(
+            `The date entered corresponds to day no. ${ordinal_date_regular_years}.`
+        );
+    } else {
+        console.log('Enter a valid number');
+    }
+}
+
+// main programm
+const day = Number(prompt('Enter an integer between 1 to 31'));
 const month = Number(prompt('Enter an integer between 1 to 12'));
 const year = Number(prompt('Enter an year'));
 
-// Check is year is a leap year and check if year is an integer
-if (
-    (year % 4 === 0 && year % 100 !== 0) ||
-    (year % 4 === 0 && year % 100 === 0 && year % 400 === 0)
-) {
-    console.log(`${year} is a leap year`);
-} else if (year === '' || year === ' ' || isNaN(year)) {
-    cl('Enter a valid integer');
-} else {
-    console.log(`${year} is not a leap year`);
-}
+gregorian_to_ordinal_date((day, month, year));
