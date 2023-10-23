67dfcbea4766e07cbe678024518c5b8f928d38a3
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-10-23 18:19:13 +0200

    Added solutions

--- projects/006-center-a-string/js/index.js
+++ projects/006-center-a-string/js/index.js
@@ -1,1 +1,24 @@
 'use strict';
+/*
+Write a function named center string that takes:
+
+string, s, as its first parameter,
+width of the window in characters, w, as its second parameter.
+Your function will return a new string that includes whatever leading spaces are needed so that s will be centered in the window when the new string is printed.
+
+The new string should be constructed in the following manner:
+
+If the length of s is greater than or equal to the width of the window then s should be returned.
+If the length of s is less than the width of the window then a string containing (len(s) - w) // 2 spaces followed by s should be returned.
+Write a main program that demonstrates your function by displaying multiple strings centered in the window.
+
+1) Come calcolare lunghezza di una stringa?
+
+2) A quanto ammonta la larghezza del Terminal Window?
+
+3) La funzione ritorna una stringa che deve essere centrata nella finestra
+
+4) Se stringa >= width terminal window ----> return stringa
+
+5) Se stringa
+*/
