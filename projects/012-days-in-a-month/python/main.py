fb27ea73a6dfc92b64cfab09db9bfc96fa4a2d60
EleSar1 <131043105+EleSar1@users.noreply.github.com>
2023-10-24 10:24:05 +0200

    Deleted ex 12

--- projects/012-days-in-a-month/python/main.py
+++ projects/012-days-in-a-month/python/main.py
@@ -1,39 +1,3 @@
-def is_leap_year(year):
-    year = int(year)
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        return True
-    else:
-        return False
-        
 
-def feb_days(year):
-    if is_leap_year(year) == True:
-        return 29
-    else:
-        return 28
-
-
-def days_in_a_month(month, year):
-    month_days = [31, feb_days(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    if 1 <= month <= 12:
-        return month_days[month - 1]
-
-
-def main():
-    year = input("Please enter a year (YYYY format): ")
-    while len(year) < 4:
-        print('Error. Year must be a four-digit input. ')
-        year = input("Please enter a year (YYYY format): ")
-    else:
-        month = int(input('Please enter a month (1-12): '))
-        if month < 1 or month > 12:
-            print('Error. Entered invalid month.')
-        else:
-            days_in_month = days_in_a_month(month, year)
-            print(f'Days in month {month}: {days_in_month}')
-
-
-if __name__ == '__main__':
-    main()
 
 
