0c5f56864a71002f9c15fb702e46a5a0938cfb4b
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-10-26 19:51:10 +0200

    added string.fromCharCode

--- projects/008-random-password/js/index.js
+++ projects/008-random-password/js/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const lenghtPassword = Math.floor(Math.random() * (10 - 7 + 1) + 7); // Set 10 as the maximum and 7 as the minimum.
+console.log(lenghtPassword);
+
+let randomPassword = ' ';
+
+for (let i = 1; i <= lenghtPassword; i++) {
+    const randomNumber = Math.floor(Math.random() * (126 - 33 + 1) + 33);
+
+    randomPassword += randomNumber + ' ';
+}
+console.log(randomPassword);
+let convertedRandomPassword = String.fromCharCode(randomPassword);
+console.log(convertedRandomPassword);
