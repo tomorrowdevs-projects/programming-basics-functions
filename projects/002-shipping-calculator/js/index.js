bde5c3521e4b86621952ee8a95af9629cac59b3c
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-10-28 17:33:58 +0200

    added solution

--- projects/002-shipping-calculator/js/index.js
+++ projects/002-shipping-calculator/js/index.js
@@ -1,32 +1,28 @@
 'use strict';
 function shippingCalculator(totalItem) {
     if (totalItem > 1) {
-        return `The total number of purchase is ${totalItem}. Total shipping is €${totalShipping.toFixed(
-            2
-        )}.`;
+        const totalShipping = (
+            (totalItem - 1) * subsequentItem +
+            firstItem
+        ).toFixed(2);
+        return `The total number of purchase is ${totalItem}. Total shipping is €${totalShipping}.`;
     } else if (
         totalItem <= 0 ||
         totalItem === '' ||
         totalItem === ' ' ||
         isNaN(totalItem)
     ) {
-        ('Enter a number greater than 0');
+        return 'Enter a number greater than 0';
     } else {
-        return alert(
-            `The total number of purchase is ${totalItem}. Total shipping is €${firstItem}.`
-        );
+        return `The total number of purchase is ${totalItem}. Total shipping is €${firstItem}.`;
     }
 }
 
-function main() {
-    let firstItem = 10.99;
-    let subsequentItem = 2.99;
-    let totalItem = Number(
-        prompt('How many items would you like to ship overall?')
-    );
-    const totalShipping = alert(
-        ((totalItem - 1) * subsequentItem + firstItem).toFixed(2)
-    );
-}
+let firstItem = 10.99;
+let subsequentItem = 2.99;
+let totalItem = Number(
+    prompt('How many items would you like to ship overall?')
+);
 
-main();
+const calculatorMessage = shippingCalculator(totalItem);
+console.log(calculatorMessage);
