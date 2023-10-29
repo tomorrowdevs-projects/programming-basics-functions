1b457da154b8ea85eb7dc16483a42a573c17e09d
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-10-29 14:07:07 +0100

    added some changes

--- projects/006-center-a-string/js/index.js
+++ projects/006-center-a-string/js/index.js
@@ -1,24 +1,22 @@
 'use strict';
 
 function centerString(s, w) {
-    let lenght_s = s.length;
-
-    const countSpaces = (w - lenght_s) / 2; // Initialize variable to count the empties spaces
-    console.log(parseInt(lenght_s));
-
     if (lenght_s >= w) {
-        console.log(s);
+        return s;
     } else {
-        console.log(countSpaces);
+        return countSpaces;
     }
-    let centeredString =
-        countSpaces > 0 ? ' '.repeat(Math.floor(countSpaces)) : '';
-    console.log(`${centeredString}${s}${centeredString}`);
 }
 
 // main programm
 let s = prompt('Enter a sentence or what you desire');
-
 let w = 80;
+let lenght_s = s.length;
+
+const countSpaces = (w - lenght_s) / 2; // Initialize variable to count the empties spaces
+console.log(parseInt(lenght_s));
+
+let centeredString = countSpaces > 0 ? ' '.repeat(Math.floor(countSpaces)) : '';
+console.log(`${centeredString}${s}${centeredString}`);
 
 centerString(s, w);
