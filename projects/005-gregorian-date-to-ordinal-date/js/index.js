3e544c55759193ff405038ad3673663dee328333
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-10-29 13:57:13 +0100

    removed alerts and constants

--- projects/005-gregorian-date-to-ordinal-date/js/index.js
+++ projects/005-gregorian-date-to-ordinal-date/js/index.js
@@ -1,15 +1,6 @@
 'use strict';
+
 function gregorian_to_ordinal_date(day, month, year) {
-    const ordinal_date_regular_years =
-        30 * (month - 1) + Math.trunc(0.6 * (month + 1)) - 3 + day; // I used the algorithm of the so-called Zeller's congruence method.
-
-    const ordinal_date_leap_years =
-        30 * (month - 1) + Math.trunc(0.6 * (month + 1)) - 2 + day;
-
-    const leapYear =
-        (year % 4 === 0 && year % 100 !== 0) ||
-        (year % 4 === 0 && year % 100 === 0 && year % 400 === 0);
-    // First condition if it is a leap year
     if (
         (day <= 31 &&
             year === leapYear &&
@@ -22,9 +13,7 @@ function gregorian_to_ordinal_date(day, month, year) {
             (month === 4 || month === 6 || month === 9 || month === 11)) ||
         (day <= 29 && month === 2 && year === leapYear)
     ) {
-        console.log(
-            `The date entered corresponds to day no. ${ordinal_date_leap_years}.`
-        );
+        return `The date entered corresponds to day no. ${ordinal_date_leap_years}.`;
         // Second condition if it is not a leap year
     } else if (
         (day <= 31 &&
@@ -38,11 +27,9 @@ function gregorian_to_ordinal_date(day, month, year) {
             (month === 4 || month === 6 || month === 9 || month === 11)) ||
         (day <= 28 && month === 2 && year !== leapYear)
     ) {
-        console.log(
-            `The date entered corresponds to day no. ${ordinal_date_regular_years}.`
-        );
+        return `The date entered corresponds to day no. ${ordinal_date_regular_years}.`;
     } else {
-        console.log('Enter a valid number');
+        return 'Enter a valid number';
     }
 }
 
@@ -51,4 +38,15 @@ const day = Number(prompt('Enter an integer between 1 to 31'));
 const month = Number(prompt('Enter an integer between 1 to 12'));
 const year = Number(prompt('Enter an year'));
 
-gregorian_to_ordinal_date(day, month, year);
+const ordinal_date_regular_years =
+    30 * (month - 1) + Math.trunc(0.6 * (month + 1)) - 3 + day; // I used the algorithm of the so-called Zeller's congruence method.
+
+const ordinal_date_leap_years =
+    30 * (month - 1) + Math.trunc(0.6 * (month + 1)) - 2 + day;
+
+const leapYear =
+    (year % 4 === 0 && year % 100 !== 0) ||
+    (year % 4 === 0 && year % 100 === 0 && year % 400 === 0);
+
+const result = gregorian_to_ordinal_date(day, month, year);
+console.log(result);
