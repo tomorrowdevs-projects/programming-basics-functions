4cfdce135cada3543b1306697a5afb7e16130527
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-10-31 17:22:22 +0100

    added solution

--- projects/009-password-validation/js/index.js
+++ projects/009-password-validation/js/index.js
@@ -1,1 +1,33 @@
-'use script';
+'use strict';
+
+function passwordValidation() {
+    if (isUpperCase && isLowerCase && hasNumber) {
+        return 'The password is valid';
+    } else {
+        return 'Insert a new password';
+    }
+}
+
+const password = prompt('Enter a password');
+const lenghtPassword = password.length;
+
+let isUpperCase = false;
+let isLowerCase = false;
+let hasNumber = false;
+
+for (let i = 0; i < lenghtPassword; i++) {
+    const checkPassword = password.charCodeAt(i);
+
+    if (checkPassword >= 65 && checkPassword <= 90) {
+        isUpperCase = true;
+    } else if (checkPassword >= 97 && checkPassword <= 122) {
+        isLowerCase = true;
+    } else if (checkPassword >= 48 && checkPassword <= 57) {
+        hasNumber = true;
+    } else {
+        console.log('Insert something');
+    }
+}
+
+const result = passwordValidation();
+console.log(result);
