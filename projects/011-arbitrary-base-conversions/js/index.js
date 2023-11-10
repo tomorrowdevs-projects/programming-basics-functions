b54ebb64d12843992e9883ad4a3ddd7205596267
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-11-10 23:30:19 +0100

    Enter solution

--- projects/011-arbitrary-base-conversions/js/index.js
+++ projects/011-arbitrary-base-conversions/js/index.js
@@ -1,1 +1,29 @@
 'use strict';
+
+function arbitraryBaseToBase10() {
+    if (base >= 2 && base <= 16 && !isNaN(inputNumber)) {
+        const resultNumber = parseInt(inputNumber, base); //
+        return `The final result is: ${resultNumber} in base 10.`;
+    } else {
+        return 'Invalid input or base';
+    }
+}
+
+function base10ToArbitraryBase() {
+    if (base >= 2 && base <= 16 && !isNaN(inputNumber)) {
+        const resultNumber2 = inputNumber.toString(base).toUpperCase();
+        return `The final result is: ${resultNumber2} in base ${base}.`;
+    } else {
+        return 'Invalid input or base';
+    }
+}
+
+// programm main
+const inputNumber = Number(prompt('Enter a number'));
+const base = Number(prompt('Enter a number between 2 and 16'));
+
+const resultBase10 = arbitraryBaseToBase10();
+console.log(resultBase10);
+
+const resultBaseArbitrary = base10ToArbitraryBase();
+console.log(resultBaseArbitrary);
