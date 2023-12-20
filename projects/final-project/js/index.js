f5ec85c6b5e55aaa1da27b41872e855b0682a70d
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-12-20 17:31:58 +0100

    Added solution

--- projects/final-project/js/index.js
+++ projects/final-project/js/index.js
@@ -1,20 +1,26 @@
 'use strict';
 
-function getSecurePassword() {
+function getSecurePassword(length) {
+    let password = '';
+
+    for (let i = 1; i <= length; i++) {
+        const randomCharachter = Math.floor(
+            Math.random() * (220 - 33 + 1) + 33
+        );
+        password += String.fromCharCode(randomCharachter);
+    }
     return password;
+}
+
+function hidePassword(password) {
+    return 'x'.repeat(password.length);
 }
 const desiredPasswordLength = Number(
     prompt('Enter the number of characters you want your password to contain')
 );
-// console.log(desiredPasswordLength);
-// console.log(typeof desiredPasswordLength);
 
-let password = '';
+const getSecurePasswordGenerated = getSecurePassword(desiredPasswordLength);
+const hiddenPassword = hidePassword(getSecurePasswordGenerated);
 
-for (let i = 1; i <= desiredPasswordLength; i++) {
-    const randomCharachter = Math.floor(Math.random() * (220 - 33 + 1) + 33);
-    password += String.fromCharCode(randomCharachter + '');
-}
-
-const generatedPassword = getSecurePassword();
-console.log(generatedPassword);
+console.log(hiddenPassword);
+console.log(getSecurePasswordGenerated);
