20152e35cd2afd5fd6b7debe5090f899b93c843e
Daniele <danieleslvstrn@gmail.com>
2024-02-05 18:12:08 +0100

    add annotations and docstrings

--- projects/032-taxi-fare/python/main.py
+++ projects/032-taxi-fare/python/main.py
@@ -1,18 +1,19 @@
-# defines a function that takes km as the only parameter to calculate the price for taxi fares
-def taxi_fares(km): 
-# transforms kilometers in meters and stores it in a variable called "meters"
+def taxi_fares(km: int)-> float: 
+    '''returns the price for a ride by taxi that costs a base fare of 4.00€ and 0.25€ every 140 meters travelled.
+            :parameter:
+                    km (int): a decimal integer.
+            :return:
+                    total (float): rounded floating point number of two digits.
+    '''
     meters = km * 1000
-# calculates the price per meter
     meters_costs = (meters / 140) * 0.25
-# calculate total price
     total = round((meters_costs + 4.00), 2)
     return total
 
-# declares a main function that use taxy_fares(km)
+
 def main(): 
     print(f'Total amount is {taxi_fares(5)} euros')
     
-
-# sets a conditional statement so the program run main() only if has not been imported in a another file.  
+ 
 if __name__ == '__main__':
     main()
