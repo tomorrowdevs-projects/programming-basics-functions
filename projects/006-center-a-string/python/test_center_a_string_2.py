4bb6de73f5499da6efb06e729dc76c3f3bdb72d6
Daniele <danieleslvstrn@gmail.com>
2024-02-07 16:29:44 +0100

    changes to test docstrings

--- projects/006-center-a-string/python/test_center_a_string_2.py
+++ projects/006-center-a-string/python/test_center_a_string_2.py
@@ -5,21 +5,21 @@ from main import center_string
 class Test_center_a_string(unittest.TestCase):
 
     def test_center_a_string_one(self):
-        '''Check whether it returns the string correctly when the inserted string is larger than the screen'''
+        '''Check that the function returns the correct result when the inserted string is longer than the screen'''
 
         result = center_string('This screen is smaller then the string', 5)
         self.assertEqual(result,'This screen is smaller then the string')
 
 
     def test_center_a_string_two(self):
-        '''Check whether it returns the string correctly when the inserted string is larger than the screen'''
-        
+        '''Check that the function returns the correct result when the inserted string has an equal length of the screen'''
+
         result = center_string('This screen is equal then the string',36)
         self.assertEqual(result,'This screen is equal then the string')
 
 
     def test_center_a_string_three(self):
-        '''Check whether it returns the string correctly when the inserted string is larger than the screen'''
+        '''Check that the function returns the correct result when the inserted string has a smaller screen length'''
 
         result = center_string('    This screen is larger then the string    ', 44)
         self.assertEqual(result,'    This screen is larger then the string    ')
