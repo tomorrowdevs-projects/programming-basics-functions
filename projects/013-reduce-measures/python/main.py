f8a0ee41927f8a50db34275262a0ca522e1b0be1
Daniele <danieleslvstrn@gmail.com>
2024-02-09 16:21:09 +0100

    fix solution,add docstrings and annotations

--- projects/013-reduce-measures/python/main.py
+++ projects/013-reduce-measures/python/main.py
@@ -1,80 +1,70 @@
-# defines a function that express an imperial volume using the largest units of measures possible between cups, tablespoons and teaspoons. 
-def reduce_measures(units, unit_of_measure):
-    result = ''
-# conditionals that modify execution of flow in base of unit of measure
-    if unit_of_measure == 'cup':
-        cup = units
-        tablespoon = 0
-        teaspoon = 0
-# conditional that correct grammar to the answer
-        if cup <= 1 :
-            result += f'{units} cup, {tablespoon} tablespoon, {teaspoon} teaspoon'
-        else: 
-            result += f'{units} cups, {tablespoon} tablespoon, {teaspoon} teaspoon'
-    elif unit_of_measure == 'tablespoon':
-# conditional that permorms computation in base of the number of units and adds step by step a string to result's variable      
-        if units > 16:
-            cup = units // 16
-            tablespoon = units - (cup * 16)
-            teaspoon = 0
-            if cup <= 1 :
-                result += f'{cup} cup,'
-            else: 
-                result += f'{cup} cups,'
-            if tablespoon <= 1:
-                result += f' {tablespoon} tablespoon, {teaspoon} teaspoon'
-            else:
-                result += f' {tablespoon} tablespoons, {teaspoon} teaspoon'
+
+def reduce_measures(units:int, unit_of_measure:str)-> str:
+    
+    '''Returns, given a quantity in a specific unit of measurement, a string representing the measure using the largest units of measures possible (between cups, tablespoons and teaspoons).
+            :parameters:
+                    units(int): the number of unit.
+                    unit_of_measures(str): the unit of measure(cup, tablespoon or teaspoon)
+            :return: 
+                    largest_unit_possible(str): a string with the largest unit/s possible.
+    ''' 
+
+    largest_units_possible = ''
+
+    # if statement that changes the execution flow according to the unit of measurement
+    if unit_of_measure == 'teaspoon':
+
+        # calculate equivalent cups and add an appropriate message to the variable
+        cup = units // 48
+        if cup <= 1:
+                largest_units_possible += f'{cup} cup, '
         else:
-            cup = 0
-            tablespoon = units
-            teaspoon = 0
-            if tablespoon <= 1:
-                result += f'{cup} cup, {tablespoon} tablespoon, {teaspoon} teaspoon'
-            else:
-                result += f'{cup} cup, {tablespoon} tablespoons, {teaspoon} teaspoon'
+            largest_units_possible += f'{cup} cups, '
 
-    elif unit_of_measure == 'teaspoon':
+        # calculate tablespoon
+        units = units % 48
+        tablespoon = units // 3
+        if tablespoon <= 1:
+            largest_units_possible += f'{tablespoon} tablespoon, '
+        else:
+            largest_units_possible += f'{tablespoon} tablespoons, '
 
-        if units > 48:
-            cup = units // 48
-            tablespoon = (units - cup * 48 ) // 3
-            teaspoon = (units - cup * 48) - ((units - cup * 48) // 3)
-            if cup <= 1 :
-                result += f'{cup} cup,'
-            else: 
-                result += f'{cup} cups,'
+        #calculate teaspoon
+        units = units % 3
+        teaspoon = units
+        if units <= 1:
+            largest_units_possible += f'{teaspoon} teaspoon'
+        else:
+            largest_units_possible += f'{teaspoon} teaspoons'
 
-            if tablespoon <= 1:
-                result += f' {tablespoon} tablespoon,'
-            else:
-                result += f' {tablespoon} tablespoons,'
+    elif unit_of_measure == 'tablespoon':
 
-            if teaspoon < 1:
-                result += f' {teaspoon} teaspoon'
-            else: 
-                result += f' {teaspoon} teaspoons'
+        cup = units // 16
+        if cup <= 1:
+                largest_units_possible += f'{cup} cup,'
+        else:
+            largest_units_possible += f'{cup} cups, '
 
-        elif 3 < units < 48:
-            cup = 0
-            tablespoon = units // 3
-            teaspoon = units - tablespoon * 3 
-            result += f' {cup} cup,'
+        units = units % 16
+        tablespoon = units
+        if tablespoon <= 1:
+            largest_units_possible += f'{tablespoon} tablespoon, 0 teaspoon'
+        else:
+            largest_units_possible += f'{tablespoon} tablespoons, 0 teaspoon'
 
-            if tablespoon <= 1:
-                result += f' {tablespoon} tablespoon,'
-            else:
-                result += f' {tablespoon} tablespoons,'
+    elif unit_of_measure == 'cup':
+        cup = units
+        if cup <= 1:
+                largest_units_possible += f'{cup} cup, 0 tablespoon, 0 teaspoon'
+        else:
+            largest_units_possible += f'{cup} cups, 0 tablespoon, 0 teaspoon'
+    
+    return largest_units_possible
 
-            if teaspoon <= 1:
-                result += f' {teaspoon} teaspoon'
-            else: 
-                result += f' {teaspoon} teaspoons'
-        elif units <= 3:
-            cup = 0
-            tablespoon = 0
-            result += f'{cup} cup, {tablespoon} tablespoon, {units} teaspoon'
- 
-    return result
 
-print(reduce_measures(73,'tablespoon'))
+def main():
+     print(reduce_measures(59,'teaspoon'))
+     
+
+if __name__ == '__main__':
+     main()
