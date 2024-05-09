870e179d6a18af4f99c7184e15fac423f394164a
Roberto <robertomoggia95@gmail.com>
2024-05-09 22:29:44 +0200

    solution/m1-003
    Second commit - Used list

--- projects/003-integer-to-ordinal/python/main.py
+++ projects/003-integer-to-ordinal/python/main.py
@@ -1,30 +1,9 @@
 def integer_to_ordinal(number):
+    ordinal_number=['first','second','third','fourth','fifth','sixth','seventh','eighth','ninth','tenth','eleventh','twelfth']
     if not number in range(1,13):
         number=''
-    if number==1:
-        number='first'
-    elif number==2:
-        number='second'
-    elif number==3:
-        number='third'
-    elif number==4:
-        number='fourth'
-    elif number==5:
-        number='fifth'
-    elif number==6:
-        number='sixth'
-    elif number==7:
-        number='seventh'
-    elif number==8:
-        number='eighth'
-    elif number==9:
-        number='ninth'
-    elif number==10:
-        number='tenth'
-    elif number==11:
-        number='eleventh'
-    elif number==12:
-        number='twelfth'
+    else:
+        number=ordinal_number[number-1]
     return number
 
 def main():
