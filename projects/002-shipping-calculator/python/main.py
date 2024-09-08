aa2911b5e72f7efd6fed4a4d2f1a70f4c56d72d9
BloodYce <mattyago99@gmail.com>
2024-09-08 11:12:38 +0200

    solution-m4/001

---
+++ projects/002-shipping-calculator/python/main.py
@@ -0,0 +1,13 @@
+'''
+An online retailer provides express shipping for many of its items at a rate of: 
+- €10.99 for the first item in an order
+- €2.99 for each subsequent item in the same order. 
+
+Write a function named **shipping calculator** that takes the number of items in the order as its **only parameter**. 
+
+Return the shipping charge for the order as the function’s result. 
+
+Include a main program that reads the number of items purchased from the user 
+and displays the shipping charge.
+'''
+
