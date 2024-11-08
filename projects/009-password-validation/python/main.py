e81b6b387ba2b63f4e24fa8c5229246f59de33b7
Natalia <natalia.muccio@gmail.com>
2024-11-08 17:24:29 +0100

    solution/m1-009

--- projects/009-password-validation/python/main.py
+++ projects/009-password-validation/python/main.py
@@ -1,43 +1,47 @@
-def is_valid_password(password):
-    has_least_eight_characters=True
-    if len(password)>7:    
-        for character in password:
-            if ord(character)<33 or ord(character)>126:
-                has_least_eight_characters=False
-    else:
+def is_valid_password(password: str) -> bool:
+    if len(password)<8:
         return False
-    if has_least_eight_characters:
-        
-        has_least_one_uppercase=False
-        index=0
-        while not(has_least_one_uppercase) and index<len(password):
-            character=password[index]
-            if ord(character)>64 and ord(character)<91:
-                has_least_one_uppercase=True
-            index+=1
 
-        has_least_one_lowercase=False
-        index=0
-        while not(has_least_one_lowercase) and index<len(password):
-            character=password[index]
-            if ord(character)>96 and ord(character)<123:
-                has_least_one_lowercase=True
-            index+=1
+    has_least_one_uppercase=False
+    has_least_one_lowercase=False
+    has_least_one_number=False
+    for character in password:
+        if ord(character)>64 and ord(character)<91:
+            has_least_one_uppercase=True
+        elif ord(character)>96 and ord(character)<123:
+            has_least_one_lowercase=True
+        elif ord(character)>47 and ord(character)<58:
+            has_least_one_number=True
+        elif ord(character)<33 or ord(character)>126:
+            return False
+           
+        # has_least_one_uppercase=False
+        # index=0
+        # while not(has_least_one_uppercase) and index<len(password):
+        #     character=password[index]
+        #     if ord(character)>64 and ord(character)<91:
+        #         has_least_one_uppercase=True
+        #     index+=1
 
-        has_least_one_number=False
-        index=0
-        while not(has_least_one_number) and index<len(password):
-            character=password[index]
-            if ord(character)>47 and ord(character)<58:
-                has_least_one_number=True
-            index+=1
+        # has_least_one_lowercase=False
+        # index=0
+        # while not(has_least_one_lowercase) and index<len(password):
+        #     character=password[index]
+        #     if ord(character)>96 and ord(character)<123:
+        #         has_least_one_lowercase=True
+        #     index+=1
+
+        # has_least_one_number=False
+        # index=0
+        # while not(has_least_one_number) and index<len(password):
+        #     character=password[index]
+        #     if ord(character)>47 and ord(character)<58:
+        #         has_least_one_number=True
+        #     index+=1
 
         if has_least_one_uppercase and has_least_one_lowercase and has_least_one_number:
             return True
-        else:
-            return False
-    else:
-        return False
+    return False
 
 def main():
     password=input("Enter a password: ")
